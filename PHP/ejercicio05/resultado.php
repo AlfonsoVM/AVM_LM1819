e68006cc0794f9286a026efe4cd3e41c3474c990
Alumno <vargasmesa.alfonso@gmail.com>
2019-02-14 10:19:44 +0100

    14-02-19

--- PHP/ejercicio05/resultado.php
+++ PHP/ejercicio05/resultado.php
@@ -1,4 +1,14 @@
 <?php
+if ($_REQUEST["nombre"]!="") {
+        print "<p>Su nombre es $_REQUEST[nombre]</p>";
+    }
+
+if (isset($_REQUEST["opcion"])) {
+    print "<p>Opción marcada</p>";
+}
+
+print "<p>Su nombre es $_REQUEST[nombre]</p>";
+
 print_r($_REQUEST);
 print_r($_FILES);
 ?>
